635dd878e0eb92b34d2f43654f6094880e92bbc6
Ted Gusek <theohgusek@gmail.com>
2023-06-21 15:39:55 -0400

    Recipes Model

---
+++ Models/Recipes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RecipeShare.Models
+{
+	public class Recipes
+	{
+		// setting unique ID for each recipe
+		public int RecipeId { get; set; }
+
+		public string Title { get; set; }
+
+		public string Instructions { get; set; }
+
+		public string Image { get; set; }
+
+		// for rating and comments of recipe
+		public double Ratings { get; set; }
+
+		public string Comments { get; set; }
+
+
+	}
+}
+
