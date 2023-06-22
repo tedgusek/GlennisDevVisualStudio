665abc139670409b237c893d5ed844d23dbcae5d
Ted Gusek <theohgusek@gmail.com>
2023-06-22 15:15:34 -0400

    adding CRUD for recipe controller

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeShare.Data;
+using RecipeShare.Models;
+
+namespace RecipeShare.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RecipeController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: /Recipe
+        public IActionResult Index()
+        {
+            // Logic to retrieve and return a list of recipes
+        }
+
+        // GET: /Recipe/Details/{id}
+        public IActionResult Details(int id)
+        {
+            // Logic to retrieve a specific recipe by ID and return it
+        }
+
+        // GET: /Recipe/Create
+        public IActionResult Create()
+        {
+            // Logic to display the create recipe form
+        }
+
+        // POST: /Recipe/Create
+        [HttpPost]
+        public IActionResult Create(Recipe recipe)
+        {
+            // Logic to create a new recipe based on the posted data
+        }
+
+        // GET: /Recipe/Edit/{id}
+        public IActionResult Edit(int id)
+        {
+            // Logic to retrieve a specific recipe by ID and display the edit form
+        }
+
+        // POST: /Recipe/Edit/{id}
+        [HttpPost]
+        public IActionResult Edit(int id, Recipe recipe)
+        {
+            // Logic to update a specific recipe based on the posted data
+        }
+
+        // GET: /Recipe/Delete/{id}
+        public IActionResult Delete(int id)
+        {
+            // Logic to retrieve a specific recipe by ID and display the delete confirmation page
+        }
+
+        // POST: /Recipe/Delete/{id}
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            // Logic to delete a specific recipe based on the confirmation
+        }
+    }
+}
