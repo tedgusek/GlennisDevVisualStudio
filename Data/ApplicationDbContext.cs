b2bcebecc8693d0b9edf4b8af4a603194d6a26f6
Ted Gusek <theohgusek@gmail.com>
2023-06-22 14:27:05 -0400

    DbContext and reverted SnapShot

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,43 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RecipeShare.Models;
 
-namespace RecipeShare.Data;
-
-public class ApplicationDbContext : IdentityDbContext
+namespace RecipeShare.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Recipes> Recipes { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Recipes>(b =>
+            {
+                b.Property(r => r.RecipeId)
+                    .ValueGeneratedOnAdd();
+
+                b.Property(r => r.Title)
+                    .IsRequired();
+
+                b.Property(r => r.Instructions)
+                    .IsRequired();
+
+                b.Property(r => r.Image);
+
+                b.Property(r => r.Ratings);
+
+                b.Property(r => r.Comments);
+
+                b.HasKey(r => r.RecipeId);
+
+                b.ToTable("Recipes");
+            });
+        }
     }
 }
-
